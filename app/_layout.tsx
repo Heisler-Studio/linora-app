840789b3dff6978b59867365855fcef51920dd6c
Evan Heisler <evanheisler@gmail.com>
2025-11-01 08:14:52 -0700

    replace base layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,41 @@
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+// import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function TabLayout() {
+export default function Layout() {
+  const colorScheme = useColorScheme() || 'light';
+
   return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="home">
-        <Label>Home</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
-      </NativeTabs.Trigger>
-    </NativeTabs>
+    <GestureHandlerRootView style={styles.container}>
+      <ActionSheetProvider>
+        <ThemeProvider
+          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+
+          <Stack>
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack>
+        </ThemeProvider>
+      </ActionSheetProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
